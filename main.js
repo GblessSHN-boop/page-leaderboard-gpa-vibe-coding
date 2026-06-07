872ad6fd652c3a674c5499fb133171273bf24d61
GLAND JERMANO BLESSED SIAHAAN <GblessSHN-boop@users.noreply.github.com>
2026-06-07 13:05:18 +0700

    Clean movement icons background

--- main.js
+++ main.js
@@ -14,20 +14,65 @@
       {
         "rank": 2,
         "name": "NO NAME",
+        "gpa": "GPA 4.00",
         "change": 1,
         "movement": "up"
       },
       {
         "rank": 3,
         "name": "NO NAME",
+        "gpa": "GPA 4.00",
         "change": 1,
         "movement": "up"
       },
       {
         "rank": 4,
         "name": "NO NAME",
+        "gpa": "GPA 4.00",
         "change": 1,
         "movement": "down"
+      },
+      {
+        "rank": 5,
+        "name": "NO NAME",
+        "gpa": "GPA 4.00",
+        "change": 1,
+        "movement": "up"
+      },
+      {
+        "rank": 6,
+        "name": "NO NAME",
+        "gpa": "GPA 4.00",
+        "change": 1,
+        "movement": "down"
+      },
+      {
+        "rank": 7,
+        "name": "NO NAME",
+        "gpa": "GPA 4.00",
+        "change": 1,
+        "movement": "up"
+      },
+      {
+        "rank": 8,
+        "name": "NO NAME",
+        "gpa": "GPA 4.00",
+        "change": 1,
+        "movement": "up"
+      },
+      {
+        "rank": 9,
+        "name": "NO NAME",
+        "gpa": "GPA 4.00",
+        "change": 1,
+        "movement": "down"
+      },
+      {
+        "rank": 10,
+        "name": "NO NAME",
+        "gpa": "GPA 4.00",
+        "change": 1,
+        "movement": "up"
       }
     ]
   },
@@ -46,27 +91,72 @@
       {
         "rank": 2,
         "name": "NO NAME",
+        "gpa": "GPA 4.00",
         "change": 1,
         "movement": "up"
       },
       {
         "rank": 3,
         "name": "NO NAME",
+        "gpa": "GPA 4.00",
         "change": 1,
         "movement": "up"
       },
       {
         "rank": 4,
         "name": "NO NAME",
+        "gpa": "GPA 4.00",
         "change": 1,
         "movement": "down"
+      },
+      {
+        "rank": 5,
+        "name": "NO NAME",
+        "gpa": "GPA 4.00",
+        "change": 1,
+        "movement": "up"
+      },
+      {
+        "rank": 6,
+        "name": "NO NAME",
+        "gpa": "GPA 4.00",
+        "change": 1,
+        "movement": "down"
+      },
+      {
+        "rank": 7,
+        "name": "NO NAME",
+        "gpa": "GPA 4.00",
+        "change": 1,
+        "movement": "up"
+      },
+      {
+        "rank": 8,
+        "name": "NO NAME",
+        "gpa": "GPA 4.00",
+        "change": 1,
+        "movement": "up"
+      },
+      {
+        "rank": 9,
+        "name": "NO NAME",
+        "gpa": "GPA 4.00",
+        "change": 1,
+        "movement": "down"
+      },
+      {
+        "rank": 10,
+        "name": "NO NAME",
+        "gpa": "GPA 4.00",
+        "change": 1,
+        "movement": "up"
       }
     ]
   }
 ];
 const movementIcons = {
-  up: "assets/icons/movement/up.png?v=2",
-  down: "assets/icons/movement/down.png?v=2"
+  up: "assets/icons/movement/up-clean.png?v=20260607130104",
+  down: "assets/icons/movement/down-clean.png?v=20260607130104"
 };
 const grid = document.querySelector("#rankingGrid");
 const filters = document.querySelector("#rankingFilters");
@@ -142,10 +232,12 @@ function createFighterRow(fighter) {
       </span>`
     : `<span class="change-pill"></span>`;
 
+  const hoverText = fighter.gpa || "GPA 4.00";
+
   return `
     <li class="fighter-row">
       <span class="rank-number">${fighter.rank}</span>
-      <span class="fighter-name">${fighter.name}</span>
+      <span class="fighter-name" data-hover="${hoverText}">${fighter.name}</span>
       ${movementMarkup}
     </li>
   `;
@@ -158,6 +250,12 @@ menuToggle.addEventListener("click", () => {
 
 renderFilters();
 renderCards();
+
+
+
+
+
+
 
 
 
